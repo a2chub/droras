6ae8b0d17113b0d649ed6495feef9f47f6a8927b
Saqoosha <a@saqoo.sh>
2019-09-05 21:51:18 +0900

    Remove unwanted API call prefix url

--- nuxt/dronas-admin/nuxt.config.js
+++ nuxt/dronas-admin/nuxt.config.js
@@ -48,6 +48,7 @@ export default {
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
+    baseURL: ""
   },
   /*
   ** Build configuration
@@ -58,5 +59,8 @@ export default {
     */
     extend (config, ctx) {
     }
+  },
+  generate: {
+    dir: "../../static"
   }
 }
