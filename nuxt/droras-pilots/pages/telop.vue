023ba7c1f06b8dfab9aed1c31a01c045e78aba13
Saqoosha <a@saqoo.sh>
2019-09-09 22:45:41 +0900

    Add telop screen

---
+++ nuxt/droras-pilots/pages/telop.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="bg">
+    <div
+      v-for="(pilot, index) in pilots"
+      :key="index"
+      class="name"
+    >
+      {{ pilot.name }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    pilots () {
+      const current = this.$store.state.current.heat | 0
+      return this.$store.state.heats.filter((heat, index) => index + 1 === current)[0]
+    }
+  },
+  created () {
+    this.$store.dispatch('initRaces')
+    this.$store.dispatch('initCurrent')
+  }
+}
+</script>
+
+<style>
+html, body, #__nuxt, #__layout {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  background-color: black;
+}
+.bg {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+}
+.name {
+  color: white;
+  width: 33.3333%;
+  text-align: center;
+  font-size: 20px;
+  font-weight: bold;
+}
+</style>
