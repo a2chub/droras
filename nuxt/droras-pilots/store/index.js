3cb8a8b24d771f115b0a929af4eeed27b9f162c5
Saqoosha <a@saqoo.sh>
2019-09-09 20:40:48 +0900

    Use firebase as realtime data store

--- nuxt/droras-pilots/store/index.js
+++ nuxt/droras-pilots/store/index.js
@@ -1,26 +1,56 @@
-export const state = () => ({
-  heats: [],
-  current: 0
-})
+import {
+  vuexfireMutations,
+  firestoreAction
+} from 'vuexfire'
+import axios from 'axios'
+import firebase from '@/plugins/firebase'
 
 export const mutations = {
-  setHeats (state, heats) {
+  SET_HEATS (state, heats) {
     state.heats = heats
   },
-  setCurrent (state, index) {
-    state.current = index
+  ...vuexfireMutations
+}
+const db = firebase.firestore()
+const currentRef = db.collection('race').doc('current')
+
+export const state = () => ({
+  current: {},
+  heats: []
+})
+
+export const getters = {
+  current: (state) => {
+    return state.current
+  },
+  heats: (state) => {
+    return state.heats
   }
 }
 
 export const actions = {
-  async nuxtServerInit ({
-    commit
-  }, {
-    app
-  }) {
-    const heats = await app.$axios.$get('/api/heat-data')
-    commit('setHeats', heats)
-    const current = await app.$axios.$get('/api/current-heat')
-    commit('setCurrent', current.id)
-  }
+  async initRaces ({ commit }) {
+    const response = await axios.get('https://script.google.com/macros/s/AKfycbwY05MtkIet6Yc_MlQvD9Ng4H_ZTpBcFZvtTj_BPE008Az8H8x2/exec?getrace=6')
+    const heats = []
+    for (const data of response.data) {
+      const [JDL_ID, name, klass, index] = data
+      let group = heats[index]
+      if (!group) {
+        group = []
+        heats[index] = group
+      }
+      group.push({
+        JDL_ID,
+        name,
+        'class': klass,
+        'heat': index
+      })
+    }
+    commit('SET_HEATS', heats.filter(g => !!g))
+  },
+  initCurrent: firestoreAction(({
+    bindFirestoreRef
+  }) => {
+    bindFirestoreRef('current', currentRef)
+  })
 }
