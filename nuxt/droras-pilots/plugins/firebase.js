677c91f4dfbac42a779c2d6efbb4c7f99a6a51fb
Saqoosha <a@saqoo.sh>
2019-09-09 21:06:51 +0900

    Cleanup unused keys

--- nuxt/droras-pilots/plugins/firebase.js
+++ nuxt/droras-pilots/plugins/firebase.js
@@ -2,13 +2,8 @@ import firebase from 'firebase'
 
 if (!firebase.apps.length) {
   firebase.initializeApp({
-    apiKey: 'AIzaSyDJLakGhgbVQhgu-AlF7_JP36KYZlOE8i8',
-    authDomain: 'jdl-main.firebaseapp.com',
-    databaseURL: 'https://jdl-main.firebaseio.com',
     projectId: 'jdl-main',
-    storageBucket: 'jdl-main.appspot.com',
-    messagingSenderId: '457977284384',
-    appId: '1:457977284384:web:bdfa2434119cb460043945'
+    apiKey: 'AIzaSyDJLakGhgbVQhgu-AlF7_JP36KYZlOE8i8'
   })
 }
 
