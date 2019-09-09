34122a92b83d3fa80c8b4a6a088005ef785796e5
a2c <a2cgle@gmail.com>
2019-09-09 21:07:01 +0900

    key 無効化2

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 source `pwd`/venv/bin/activate
-export GOOGLE_APPLICATION_CREDENTIALS=`pwd`"/jdl_lib/jdl-main-b84eea70963d.json"
+export GOOGLE_APPLICATION_CREDENTIALS=`pwd`"/jdl_lib/jdl-main-key.json"
 python main.py
