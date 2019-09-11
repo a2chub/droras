a782d6d192fbb5569e4b4abae71ffe3393660af9
Saqoosha <a@saqoo.sh>
2019-09-11 16:15:40 +0900

    Add class title

--- nuxt/droras-pilots/pages/index.vue
+++ nuxt/droras-pilots/pages/index.vue
@@ -9,14 +9,19 @@
           <th>F4: 5800</th>
         </tr>
       </thead>
-      <tbody id="list">
+      <tbody v-for="(klass, index) of heats" :key="index" class="list">
+        <tr>
+          <td colspan="4" class="class-name">
+            {{ klass.class }}
+          </td>
+        </tr>
         <tr
-          v-for="(heat, index) in $store.state.heats"
-          :key="index"
-          :class="{ 'table-success': index + 1 == $store.state.current.heat }"
+          v-for="heat in klass.heats"
+          :key="heat.index"
+          :class="{ 'table-success': heat.active }"
         >
-          <td>{{ index + 1 }}</td>
-          <td v-for="(pilot, pilotindex) in heat" :key="pilotindex">
+          <td>{{ heat.index }}</td>
+          <td v-for="(pilot, pilotindex) in heat.pilots" :key="pilotindex">
             {{ pilot.JDL_ID.match(/^J/) ? pilot.name : "-" }}
           </td>
         </tr>
@@ -27,9 +32,33 @@
 
 <script>
 export default {
+  computed: {
+    heats () {
+      const current = this.$store.state.current.heat | 0
+      const allHeats = { 'open': [], 'expert': [], 'pro': [] }
+      for (const [index, pilots] of this.$store.state.heats.entries()) {
+        const klass = pilots.find(p => p.name).class.toLowerCase()
+        allHeats[klass].push({ index: index + 1, active: index + 1 === current, pilots })
+      }
+      return [
+        { 'class': 'Open', heats: allHeats.open },
+        { 'class': 'Expert', heats: allHeats.expert },
+        { 'class': 'Pro', heats: allHeats.pro }
+      ]
+    }
+  },
   created () {
     this.$store.dispatch('initRaces')
     this.$store.dispatch('initCurrent')
   }
 }
 </script>
+
+<style scoped>
+th, td {
+  text-align: center;
+}
+.class-name {
+  font-weight: bold;
+}
+</style>
