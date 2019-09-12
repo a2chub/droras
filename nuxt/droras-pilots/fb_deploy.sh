4c73865038ae50f3ba3626d7a132e19323de1558
Saqoosha <a@saqoo.sh>
2019-09-12 10:52:16 +0900

    Update deploy script

--- nuxt/droras-pilots/fb_deploy.sh
+++ nuxt/droras-pilots/fb_deploy.sh
@@ -1,7 +1,5 @@
+#!/bin/bash
 
-
-
-rm  public/index.html
-cp -r dist/* public
+cp -rf ../../static/ public
 wget "https://script.google.com/macros/s/AKfycbwY05MtkIet6Yc_MlQvD9Ng4H_ZTpBcFZvtTj_BPE008Az8H8x2/exec?getrace=now" -O public/pilots.json
 firebase deploy
