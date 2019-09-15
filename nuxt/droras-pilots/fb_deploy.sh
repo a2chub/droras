e05cdc8dc2894bc35e6601e72564eed5a6cf142f
a2c <a2cgle@gmail.com>
2019-09-15 14:10:15 +0900

    final data

--- nuxt/droras-pilots/fb_deploy.sh
+++ nuxt/droras-pilots/fb_deploy.sh
@@ -2,4 +2,6 @@
 
 cp -rf ../../static/ public
 wget "https://script.google.com/macros/s/AKfycbwY05MtkIet6Yc_MlQvD9Ng4H_ZTpBcFZvtTj_BPE008Az8H8x2/exec?getrace=now" -O public/pilots.json
+
+cp public/pilots.json ../../static/pilots.json
 firebase deploy
