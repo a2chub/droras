f24ff27cdbf54fab7a20cd55e4d5f2677b5c877d
a2c <a2cgle@gmail.com>
2021-04-14 14:56:44 +0900

    起動方法を追加、サーバーのみ、データ取得＆サーバ起動の2つをファイルを分離 "

---
+++ data-run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source `pwd`/venv/bin/activate
+export GOOGLE_APPLICATION_CREDENTIALS=`pwd`"/jdl_lib/jdl-main-key.json"
+
+
+wget "https://docs.google.com/spreadsheets/d/e/2PACX-1vQSIb1_Hys9ai97Mlf9LnxHaSFdWciZ2IC9kCTMbEQAHe7lvuM-7D-_8iKOtwibWMFL1ff1bP-keLJm/pub?gid=554938252&single=true&output=csv" -O  static/pilots.csv
+
+python main.py
