83163b11d81d41f01d64556f7af754bb05d96315
a2c <a2cgle@gmail.com>
2023-06-08 22:10:07 +0900

    整理整頓

--- run.sh
+++ run.sh
@@ -3,9 +3,4 @@
 source `pwd`/venv/bin/activate
 export GOOGLE_APPLICATION_CREDENTIALS=`pwd`"/jdl_lib/jdl-main-key.json"
 
-#2020 csv
-#wget "https://docs.google.com/spreadsheets/d/e/2PACX-1vT70EjI5FcC7nNBO7DuwQeYlzADbWYWSxpO97PVHHYCUEpdsNwGGArXAh9EBpQl_3vXLyygdJZMY10K/pub?gid=554938252&single=true&output=csv" -O static/pilots.csv
-
-#wget "https://docs.google.com/spreadsheets/d/e/2PACX-1vQSIb1_Hys9ai97Mlf9LnxHaSFdWciZ2IC9kCTMbEQAHe7lvuM-7D-_8iKOtwibWMFL1ff1bP-keLJm/pub?gid=554938252&single=true&output=csv" -O  static/pilots.csv
-
 python main.py
