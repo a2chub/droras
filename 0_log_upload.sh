93710dd89e4feb249ca349bbe30b4d65476b31d1
a2c <a2cgle@gmail.com>
2023-06-08 21:20:38 +0900

    スタートタイミングを記録

---
+++ 0_log_upload.sh
@@ -0,0 +1,1 @@
+rclone copy -P ./jdl_lib/log gDrive:Drone/JDLファイル/設備関係/droras_log
--- jdl_lib/jdl_api.py
+++ jdl_lib/jdl_api.py
@@ -2,7 +2,10 @@ from fastapi import FastAPI, Request, Depends, BackgroundTasks
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
 from webapp import start_sound
+from start_log import logger
+
 app = FastAPI()
+CURRENT_HEAT_INDEX = 1
 
 try:
     from google.cloud import firestore
@@ -14,17 +17,22 @@ except:
     db = ""
     pass
 
+# リレーの制御とスター音を鳴らす
 @app.get('/api/start')
 async def start():
+    global CURRENT_HEAT_INDEX
+    logger.info(f"{CURRENT_HEAT_INDEX}\tstart_heat")
     await count_down()
     return {"status":200}
 
-
 async def count_down():
-  start_sound()
+    start_sound()
 
 @app.get('/api/{heat_index}')
 async def set_current(heat_index: int = 1):
+    global CURRENT_HEAT_INDEX
+    CURRENT_HEAT_INDEX = heat_index
+    logger.info(f"{CURRENT_HEAT_INDEX}\tchange_heat")
     try:
         print("call set_cur_heat_fb")
         await set_cur_heat_fb(str(heat_index))
@@ -39,5 +47,5 @@ async def set_cur_heat_fb(heat_id=1):
     })
 
 
-
 app.mount("/", StaticFiles(directory="../static"), name="static")
+
