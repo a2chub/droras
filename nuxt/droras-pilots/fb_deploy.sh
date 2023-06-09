08c4d4e847f29ca0fa85a0d4f13fdf357497744d
a2c <a2cgle@gmail.com>
2023-06-09 13:10:33 +0900

    未使用処理を削除

--- nuxt/droras-pilots/fb_deploy.sh
+++ nuxt/droras-pilots/fb_deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cp -rf ../../static/ public
+#cp -rf ../../static/ public
 #wget "https://script.google.com/macros/s/AKfycbwY05MtkIet6Yc_MlQvD9Ng4H_ZTpBcFZvtTj_BPE008Az8H8x2/exec?getrace=now" -O public/pilots.json
-wget "https://docs.google.com/spreadsheets/d/e/2PACX-1vT70EjI5FcC7nNBO7DuwQeYlzADbWYWSxpO97PVHHYCUEpdsNwGGArXAh9EBpQl_3vXLyygdJZMY10K/pub?gid=554938252&single=true&output=csv" -O public/pilots.csv
+#wget "https://docs.google.com/spreadsheets/d/e/2PACX-1vT70EjI5FcC7nNBO7DuwQeYlzADbWYWSxpO97PVHHYCUEpdsNwGGArXAh9EBpQl_3vXLyygdJZMY10K/pub?gid=554938252&single=true&output=csv" -O public/pilots.csv
 
-cp public/pilots.csv ../../static/pilots.csv
+#cp public/pilots.csv ../../static/pilots.csv
 firebase deploy
