5f0c11c0d04add6ac178edaf7b9a770874244144
Saqoosha <a@saqoo.sh>
2024-04-17 00:49:33 +0900

    🩹 Move initializiig from shell script to __main__.py

---
+++ run.sh
@@ -0,0 +1,3 @@
+export GOOGLE_APPLICATION_CREDENTIALS=$(pwd)/jdl-main-key.json
+
+$(pwd)/.venv/bin/python -m droras
--- run_api.sh
+++
@@ -1,6 +0,0 @@
-export GOOGLE_APPLICATION_CREDENTIALS=$(pwd)"/jdl-main-key.json"
-source $(pwd)/venv/bin/activate
-
-cd jdl_lib
-python convert_heatlist.py
-uvicorn jdl_api:app --reload --host=0.0.0.0
