e9da039a8f0093e1c9aaa4e0ad318879923bbada
Saqoosha <a@saqoo.sh>
2024-05-01 00:46:59 +0900

    🩹 Add shebang

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+# Move to the directory where this script is located
+cd "$(dirname "$0")"
+
 export GOOGLE_APPLICATION_CREDENTIALS=$(pwd)/jdl-main-key.json
 
 $(pwd)/.venv/bin/python -m droras
